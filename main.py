5344cb28233a243412c6cba981a16f83b944a759
TheOneTEM <104734823+TheOneTEM@users.noreply.github.com>
2022-05-27 21:46:33 +0200

    comments + manual bruteforce decode

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
 #Ceasar cipher encoder/decoder
-def CeasarCode(code: str, encode: bool) -> str:
+def CeasarCode(code: str, encode: bool, num) -> str:
     #reads words.txt
     from pathlib import Path
     p = Path(__file__).with_name('words.txt')
     with p.open('r') as w:
         wo=w.read()
     words=wo.split('\n')#   this returns a **list** containing the 1000 most common English words, plus some words that I added
+    
     #converts input into list of strs
     code=code.lower()
     for letter in code:
         if letter not in 'abcdefghijklmnopqrstuvwxyz':
             code.replace(letter,'')
     codelist=code.split()
+    
+    #decoding
     if not encode:
-        print()    
+        #manual decode
+        for i in range(26):
+            print('Attempt ' + str(i+1) + ': ' + CeasarCode(code, True, i+1))
+            DidItWork=input('Is it decoded? [(Y)es/(N)o]\n>')
+            while DidItWork.lower()[0] not in 'yn':
+                DidItWork=input('Input invalid. Is it decoded? [(Y)es/(N)o]\n>')
+            if DidItWork.lower()[0] == 'y':
+                return ('Decoding successful with '+str(i+1)+'attempts. Plaintext: '+ CeasarCode(code, True, i+1))
+            elif DidItWork.lower()[0] == 'n':
+                print('Continuing manual decoding.')
+        return 'Decoding failed.'
+
+    #encoding
     elif encode:
-        num = input('Input how many steps to shuffle:\n>')
-        while type(num) is not int:
-            try:
-                num=int(num)
-            except:
-                num = input('Input invalid. Try again:\n>')
+        if num == None:
+            num = input('Input amount of steps to shift:\n>')
+            while type(num) is not int:
+                try:
+                    num=int(num)
+                    while num > 25:
+                        num = input('Unable to shift more than 25 steps. Try again:\n>')
+                except:
+                    num = input('Input invalid. Input amount of steps to shift:\n>')
         tempcode=''
         letters = 'abcdefghijklmnopqrstuvwxyz'
         codeletters = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
@@ -33,6 +51,8 @@ def CeasarCode(code: str, encode: bool) -> str:
                 continue
             tempcode+=codeletters[letters.find(code[i])+num]
         return tempcode
+
+#main loop
 print('Welcome to CeasarEncode!')
 while True:
     e=input('(E)ncode or (D)ecode?\n>')
@@ -50,7 +70,4 @@ while True:
                 encode = False
                 break
     code=input('Input code:\n>')
-    print('Operation successful. Output: '+CeasarCode(code,encode))
-
-
-    
+    print('Operation completed. Output: '+CeasarCode(code, encode, None))
